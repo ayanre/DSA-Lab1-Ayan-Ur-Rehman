2d663b6a71e984537add099d1d7847418046a302
ayanre <rehmanayaanyt@gmail.com>
2025-09-15 12:00:36 +0500

    The key and array problem (task 7)

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,31 @@
-#include <iostream>
+#include<iostream>
+#include<vector>
 using namespace std;
 
+vector<int> counter(int arr[], int size, int key);
+
 int main() {
-    cout << "Welcome to DSA Lab 1!" << endl;
-    return 0;
+    const int size = 5;    
+    int arr[size] = {2, 7, 2, 3, 2};
+    int key = 2;
+
+    vector<int> newVec = counter(arr, size, key);
+
+    for(int y = 0; y<newVec.size(); y++){
+        cout << newVec.at(y) << " ";
+    }
+
+}
+
+vector<int> counter(int arr[], int size, int key){
+
+    vector<int> intV;
+
+    for(int x=0; x< size; x++){
+        if(arr[x] == key){
+            intV.push_back(x);
+        }
+    }
+
+    return intV;
 }
